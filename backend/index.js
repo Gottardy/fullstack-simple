5eb26a7ecab95d18a86240b8e11a5cc291086475
Gottardy <gottgab@gmail.com>
2023-03-25 09:25:47 -0500

    comfiguracion del servidor

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const morgan = require('morgan');
+const multer = require('multer');
+const path = require('path');
 
 // Initializations
 const app = express();
@@ -6,7 +9,20 @@ const app = express();
 // settings
 app.set('port',3000);
 
-// star server
+// middlewares
+app.use(morgan('dev'));
+
+const storageImagen = multer.diskStorage({
+    destination:path.join(__dirname,'public/uploads'),
+    filename(req,file,cb){
+        cb(null, new Date().getTime() + path.extname(file.originalname))
+    }
+})
+app.use(multer({storageImagen}).single('image'));
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+
+// start server
 app.listen(app.get('port'), ()=>{
     console.log('Server On Port', app.get('port'));
 })
