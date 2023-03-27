d951fd86d3f59e6d9711befb4570383748dfd0ac
Gottardy <gottgab@gmail.com>
2023-03-27 18:23:31 -0500

    creando los servicios con el backend

---
+++ frontend/services/BookServices.js
@@ -0,0 +1,33 @@
+class BookService{
+    constructor(){
+        this.URI = 'http://localhost:8081/api/books'
+    }
+
+    async getBooks(){
+        const response = await fetch(this.URI);
+        const books = await response.json();
+        return books;
+    }
+
+    async postBook(book){
+        const response = await fetch(this.URI,{
+            method:'POST',
+            body: book
+        });
+        const data = await response.json();
+        console.log(data);
+    }
+
+    async deleteBook(bookID){
+        const response = await fetch(`${this.URI}/${bookID}`,{
+            headers:{
+                'Content-Type':'application/json'
+            },
+            method:'DELETE'
+        });
+        const data = await response.json();
+        return data;
+    }
+}
+
+module.exports = BookService
