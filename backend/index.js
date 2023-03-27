c95f5de7cc6e313cd4a8bd8989d39770da79ea0d
Gottardy <gottgab@gmail.com>
2023-03-27 18:26:04 -0500

    mejorando la funcionalidad de cargar los imagenes al directorio uploads

--- backend/index.js
+++ backend/index.js
@@ -6,11 +6,14 @@ if(process.env.NODE_ENV!== `production`){
 const express = require('express');
 const morgan = require('morgan');
 const multer = require('multer');
-const path = require('path');
+const { extname, join } = require('path');
+
+const cors = require('cors');
 
 
 // Initializations
 const app = express();
+
 require('./bd');
 
 // settings
@@ -19,24 +22,26 @@ app.set('port',process.env.PORT || 3000);
 
 // middlewares
 app.use(morgan('dev'));
-
-const storageImagen = multer.diskStorage({
-    destination:path.join(__dirname,'public/uploads'),
-    filename(req,file,cb){
-        cb(null, new Date().getTime() + path.extname(file.originalname))
-    }
-})
-app.use(multer({storageImagen}).single('image'));
+const storage = multer.diskStorage({
+    destination: join(__dirname, "public/uploads"),
+    filename(req, file, cb) {
+      cb(null, new Date().getTime() + extname(file.originalname));
+    },
+  });
+app.use(multer({ storage }).single("image"));
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
+app.use(cors());
 
 // Routes
 app.use('/api/books',require('./routes/books'));
 
 
 // Static Files
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(join(__dirname,'public')));
 
+// uploads
+app.use("/uploads", express.static(join(__dirname, "public/uploads")));
 
 // start server
 app.listen(app.get('port'), ()=>{
